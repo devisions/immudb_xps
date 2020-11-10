0bb9eadcecb7546d3eac537d57355ae2c6f3f732
devisions <marius.ileana@gmail.com>
2020-11-10 14:48:41 +0200

    start_immudb_comps script upd

--- start_immudb_comps.sh
+++ start_immudb_comps.sh
@@ -3,9 +3,8 @@
 ## __________________________________________________________________
 ## 
 ## file: start_immudb_comps.sh
-## meta: 2020-10-02 | 2nd version: working now
 ## info: This shell script starts the two main server side components
-##       (immudb and immugw) as Docker containers
+##       (immudb and immugw) as Docker containers.
 ## __________________________________________________________________
 ## 
 
@@ -13,25 +12,30 @@
 ## using something like: docker network ls | grep immudb_net | wc -l
 
 echo
-echo ">>> Creating immudb_net network ..."
-echo
-docker network create immudb_net
-
-echo
-echo ">>> Starting immudb Server ..."
+echo ">>> Creating prereqs: immudb_xps_net network and immudb_xps_data volume ..."
 echo
 
-docker run -it -d --name immudb -p 3322:3322 -p 9497:9497             \
-       -v "$(pwd)/.immudb_data:/var/lib/immudb" --network immudb_net \
+docker network create immudb_xps_net
+
+docker volume create immudb_xps_data
+
+echo
+echo ">>> Starting immudb Server container ..."
+echo
+
+docker run -it -d --name immudb -p 3322:3322 -p 9497:9497   \
+       -v immudb_xps_data:/var/lib/immudb                   \
+       --network immudb_xps_net                             \
        codenotary/immudb:latest
 
 sleep 3
 
 echo
-echo ">>> Starting immudb Gateway ..."
+echo ">>> Starting immudb Gateway container ..."
 echo
 
-docker run -it -d -p 3323:3323 --name immugw                   \
-       --env IMMUGW_IMMUDB_ADDRESS=immudb --network immudb_net \
+docker run -it -d -p 3323:3323 --name immugw    \
+       --env IMMUGW_IMMUDB_ADDRESS=immudb       \
+       --network immudb_xps_net                 \
        codenotary/immugw:latest
 
