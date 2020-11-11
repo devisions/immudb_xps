68d85a1ebf1ccf9d886da6b4f1dfbe152df8aeb7
devisions <marius.ileana@gmail.com>
2020-11-11 11:50:41 +0200

    start script improvements

--- start_immudb_comps.sh
+++ start_immudb_comps.sh
@@ -8,27 +8,50 @@
 ## __________________________________________________________________
 ## 
 
-## TODO: Consider checking first if not already exists
-## using something like: docker network ls | grep immudb_net | wc -l
+
+NET_NAME=immudb_xps_net
+VOL_NAME=immudb_xps_vol
+
 
 echo
-echo ">>> Creating prereqs: immudb_xps_net network and immudb_xps_data volume ..."
+echo ">>> Creating prereqs: ${NET_NAME} network and ${VOL_NAME} volume ..."
 echo
 
-docker network create immudb_xps_net
 
-docker volume create immudb_xps_data
+NETCOUNT=`docker network ls | grep ${NET_NAME} | wc -l`
+
+if [ $NETCOUNT -gt 0 ]; then
+    echo ">>> Network ${VOL_NAME} already exists. Skipped creating it."
+else
+    docker network create ${NET_NAME}
+    echo ">>> Network ${NET_NAME} was created."
+fi
+
+
+VOLCOUNT=`docker volume ls | grep ${VOL_NAME} | wc -l`
+
+if [ $VOLCOUNT -gt 0 ]; then
+    echo ">>> Volume ${VOL_NAME} already exists. Skipped creating it."
+else
+    docker volume create ${VOL_NAME}
+    echo ">>> Volume ${VOL_NAME} was created."
+fi
+
 
 echo
 echo ">>> Starting immudb Server container ..."
 echo
 
 docker run -it -d --name immudb -p 3322:3322 -p 9497:9497   \
-       -v immudb_xps_data:/var/lib/immudb                   \
-       --network immudb_xps_net                             \
+       -v ${VOL_NAME}:/var/lib/immudb                       \
+       --network ${NET_NAME}                                \
        codenotary/immudb:latest
 
+## Sleeping (to give time for the server to be available) 
+## is one working approach. However, a better one is to
+## periodically check for the readiness of the server.
 sleep 3
+
 
 echo
 echo ">>> Starting immudb Gateway container ..."
@@ -36,6 +59,6 @@ echo
 
 docker run -it -d -p 3323:3323 --name immugw    \
        --env IMMUGW_IMMUDB_ADDRESS=immudb       \
-       --network immudb_xps_net                 \
+       --network ${NET_NAME}                    \
        codenotary/immugw:latest
 
